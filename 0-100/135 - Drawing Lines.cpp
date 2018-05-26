da2b9094f38aadd9916a1a0391b75c6f44c9e400
Alex Lungu <lungualex00@gmail.com>
2018-05-26 15:44:39 +0300

    SGU

---
+++ 0-100/135 - Drawing Lines.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <stack>
+
+using namespace std;
+typedef long long ll;
+
+ll n, a, b;
+
+int main()
+{
+	cin >> n;
+	cout << n*2+(n-2)*(n-1)/2;
+	return 0;
+}
